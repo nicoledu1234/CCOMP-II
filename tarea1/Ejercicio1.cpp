c0341d59d5e481abcbf5151a2c8c8123fc3cb8c4
nicoledu1234 <42581259+nicoledu1234@users.noreply.github.com>
2019-08-30 22:53:37 -0500

    Update Ejercicio1.cpp

--- tarea1/Ejercicio1.cpp
+++ tarea1/Ejercicio1.cpp
@@ -1,32 +1,51 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
-void vuelto(double numero){
-double billetes[]={200,100,50,20,10,5,2,1,0.50,0.20,0.10};
+
+void vuelto(float var){
+
+    int entero= static_cast<int>(var);
+    float decimalFloat= var*10;
+    int decimal=decimalFloat-(entero*10);
+
+    int billetes[]={200,100,50,20,10,5,2,1};
+    int centimos[]={5,2,1};
     string arr[]={" moneda de "," monedas de "};
     string arr1[]={" billete de "," billetes de "};
-    for(int i=0;i<=10;i++){
-        int temporal=numero/billetes[i];
-            if(temporal==1){
-                if(i>=4){
-                    cout<< temporal << arr[0]<< billetes[i]<<endl;
-                }
-                else {
-                    cout<< temporal << arr1[0]<< billetes[i]<<endl;
-                }
+    for(int i=0;i<=7;i++){
+        int temporal=entero/billetes[i];
+        if(temporal==1){
+            if(i>=4){
+                cout<< temporal << arr[0]<< billetes[i]<<endl;
             }
-            else{
-                if(i>=4){
-                    cout<< temporal << arr[1]<< billetes[i]<<endl;
-                }
-                else {
-                    cout<< temporal << arr1[1]<< billetes[i] <<endl;}
-                }
-            numero=numero-temporal*billetes[i];
+            else {
+                cout<< temporal << arr1[0]<< billetes[i]<<endl;
+            }
         }
+        else{
+            if(i>=4){
+                cout<< temporal << arr[1]<< billetes[i]<<endl;
+            }
+            else {
+                cout<< temporal << arr1[1]<< billetes[i] <<endl;}
+        }
+        entero= entero%billetes[i];
+    }
+
+    for(int i=0;i<=2;i++){
+        int temporalCentimos=decimal/centimos[i];
+        if(temporalCentimos==1){
+            cout<< temporalCentimos << arr[0]<< "0."<< centimos[i]<<endl;
+        }
+        else{
+            cout<< temporalCentimos << arr[1]<< "0."<< centimos[i]<<endl;
+        }
+        decimal=decimal%centimos[i];
+    }
 }
-int main()
-{
-    vuelto(1750.20);
-    return 0;
+int main(){
+    cout<<"Ingresa un numero decimal"<<endl;
+    float a;
+    cin>>a;
+    vuelto(a);
 }
